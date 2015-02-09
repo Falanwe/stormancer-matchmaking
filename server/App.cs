6eb8883d74b845366c21881558b1bce6f4076ea3
jmderuty <jmderuty@stormancer.com>
2015-02-09 15:58:54 +0100

    Updated test app for new Stormancer version

--- server/App.cs
+++ server/App.cs
@@ -4,7 +4,14 @@ namespace Test
     {
         public void Build(IAppBuilder builder)
         {
-
+            builder.SceneTemplate("test-template", scene =>
+            {
+                scene.AddRoute("echo.in", p =>
+                {
+                    scene.Broadcast("echo.out", s => p.Stream.CopyTo(s), Core.PacketPriority.MEDIUM_PRIORITY, Core.PacketReliability.RELIABLE);
+                });
+            },
+            new Dictionary<string, string> { { "description", "Broadcasts data sent to the route 'echo.in' to all connected users on the route 'echo.out'." } });
         }
     }
 }
