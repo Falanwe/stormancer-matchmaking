3d1f560c02e1b9f3a801e043002de26df3bcfd43
Jean-Noël Gourdol <jngourdol@stormancer.com>
2015-11-27 17:54:11 +0100

    added missing files in previous commit

---
+++ Configuration/ConfigurationManagerPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Stormancer.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stormancer.Server;
+
+namespace Stormancer.Configuration
+{
+    public class ConfigurationManagerPlugin : IHostPlugin
+    {
+        public void Build(HostPluginBuildContext ctx)
+        {
+            ctx.HostStarting += RegisterHost;
+        }
+
+        private static void RegisterHost(IHost host)
+        {
+            var service = new ConfigurationService(host);
+            host.DependencyResolver.Register(service);
+        }
+    }
+}
---
+++ Matchmaking/IMatchmakingContext.cs
@@ -0,0 +1,25 @@
+﻿using Stormancer.Plugins;
+using System;
+
+namespace Stormancer.Matchmaking
+{
+    public interface IMatchmakingContext<TData, TMatchFound, TRejection>
+    {
+        RequestContext<IScenePeerClient> Request { get; }
+
+        DateTime CreationTimeUTC { get; } 
+
+        TData Data { get; set; }
+
+        bool MatchFound { get; }
+        TMatchFound MatchFoundData { get; }
+
+        bool Rejected { get; }
+        TRejection RejectionData { get; }
+
+        bool IsResolved { get; }
+
+        void Success(TMatchFound successData);
+        void Fail(TRejection failureData);
+    }
+}
